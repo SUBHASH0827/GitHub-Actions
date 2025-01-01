07362793d8d32b19c17ed3b4d606134a45579857
SUBHASH0827 <76727748+SUBHASH0827@users.noreply.github.com>
2025-01-01 09:59:24 +0530

    This commit is done by Subhash to check GitHub Actions

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,10 @@
 # app.py
 # This is a test commit
+# This commit is done by Subhash
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+    assert add(10, 20) == 30
